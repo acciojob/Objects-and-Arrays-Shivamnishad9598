7184c42d26afb15693e267b397ed255b90a6136b
Shivam Kumar Nishad <111196780+Shivamnishad9598@users.noreply.github.com>
2023-08-10 10:16:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const players = ["Messi" , "Ronaldo" , "Neymar"];
-const person ={
-	name: = "Lionel Messi",
-	age: 34,
-	nationality: "Argentina"
+const players = ["Messi", "Ronaldo", "Neymar"];
+const person = {
+  name: "Lionel Messi",
+  age: 34,
+  nationality: "Argentina"
 };
 
-// Create a variable team that stores a reference to palyers
-const team players;
+// Create a variable team that stores a reference to players
+const team = players;
 
-// Create a variable team1 that is a copy of players 
+// Create a variable team1 that is a copy of players
 const team1 = players.slice();
 
-// Create a variable cap1 that is a copy of person 
+// Create a variable cap1 that is a copy of person
 const cap1 = person;
