f01c90b1bdaace8b2b07f52c69c9b0b83352e14d
Shivam Kumar Nishad <111196780+Shivamnishad9598@users.noreply.github.com>
2023-08-10 10:18:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const players = ["Messi", "Ronaldo", "Neymar"];
-const person = {
-  name: "Lionel Messi",
-  age: 34,
-  nationality: "Argentina"
+// Given array and object
+let players = ['Alice', 'Bob', 'Charlie'];
+let person = {
+  name: 'John',
+  age: 30,
+  country: 'USA'
 };
 
-// Create a variable team that stores a reference to players
-const team = players;
+// Create a variable team that refers to the players array
+let team = players;
 
-// Create a variable team1 that is a copy of players
-const team1 = players.slice();
+// Create a variable team1 that is a copy of the players array
+let team1 = players.slice(); // Using slice() to create a shallow copy of the array
 
-// Create a variable cap1 that is a copy of person
-const cap1 = person;
+// Create a variable cap1 that is a copy of the person object
+let cap1 = Object.assign({}, person); // Using Object.assign() to create a shallow copy of the object
+
+// Test
+console.log(team);   // Output: ['Alice', 'Bob', 'Charlie']
+console.log(team1);  // Output: ['Alice', 'Bob', 'Charlie']
+console.log(cap1);   // Output: { name: 'John', age: 30, country: 'USA' }
